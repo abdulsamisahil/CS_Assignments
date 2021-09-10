048e8ea162570f1e7057b923be1b07e80ebb535a
abdulsamisahil <62578384+abdulsamisahil@users.noreply.github.com>
2021-09-10 12:04:02 +0200

    savingCommerc

--- Assignment 1/Real Estate Agent/Commercials/Commercial.cs
+++ Assignment 1/Real Estate Agent/Commercials/Commercial.cs
@@ -7,7 +7,37 @@ namespace Real_Estate_Agent
 {
     public abstract class Commercial : Estate
     {
-        public override double area()
+
+        // Common vars for commercial 
+        private string usage;
+        private double income;
+        
+        // Constructor 
+
+        public Commercial (string usage, double income): base()
+        {
+            this.usage = usage;
+            this.income = income; 
+        }
+
+        public Commercial()
+        {
+
+        }
+
+        #region properties 
+        public string Usage
+        {
+            get; set;  
+        }
+        public string Income
+        {
+            get; set;
+        }
+        #endregion
+
+
+        public override double calculateArea()
         {
             throw new NotImplementedException();
         }

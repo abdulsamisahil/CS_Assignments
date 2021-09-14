70c2b0e6f22ca1d035ee65ee3b6779cfb4208774
Yousef Abdulkarim <38055561+joseDaKing@users.noreply.github.com>
2021-09-14 14:00:41 +0200

    Changes

---
+++ Assignment 1/Real Estate Agent/Program/Main/State.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Real_Estate_Agent.Program.Main
+{
+    class State<T>
+    {
+        private T _value;
+
+        public State(T value) {
+
+            _value = value;
+        }
+
+        public T value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                _value = value;
+            }
+        }
+    }
+}

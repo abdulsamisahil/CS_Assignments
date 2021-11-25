c54c6c2e02b5166af0a358b0303196c4c253d640
abdulsamisahil <62578384+abdulsamisahil@users.noreply.github.com>
2021-11-25 17:57:50 +0100

    entity version fixed

--- Assignment 4/Blackjack.backup/Program.cs
+++ Assignment 4/Blackjack.backup/Program.cs
@@ -16,7 +16,6 @@ namespace Blackjack
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             
             Application.Run(new Form1());
